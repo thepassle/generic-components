0d3a6b3ba2a3bfacf658aaa54dc3995b97dbe7d1
thepassle <pascalschilp@gmail.com>
2020-04-27 15:00:57 +0200

    chore: tests

--- generic-tooltip/test/generic-tooltip.test.js
+++ generic-tooltip/test/generic-tooltip.test.js
@@ -4,7 +4,8 @@ import '../../generic-tooltip.js';
 describe('generic-tooltip', () => {
   it('a11y', async () => {
     const el = await fixture(html`
-      <generic-tooltip></generic-tooltip>
+      <div aria-describedby="tooltipTarget"></div>
+      <generic-tooltip id="test"></generic-tooltip>
     `);
 
     expect(el).to.be.accessible();

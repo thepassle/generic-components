84df820e04555f54210f912d0fdb940c03eeca18
thepassle <pascalschilp@gmail.com>
2020-04-28 15:39:16 +0200

    fix: move keydown listener to tablist

--- generic-tabs/GenericTabs.js
+++ generic-tabs/GenericTabs.js
@@ -49,9 +49,8 @@ export class GenericTabs extends HTMLElement {
       this.__activeItem = 0;
     }
 
-    this.addEventListener('keydown', this._onKeyDown.bind(this));
-
     this.tablist = this.shadowRoot.querySelector('div[role="tablist"]');
+    this.tablist.addEventListener('keydown', this._onKeyDown.bind(this));
 
     this.tablist.setAttribute('aria-label', this.getAttribute('label') || 'tablist');
     this.tablist.addEventListener('click', this._onTabClicked.bind(this));

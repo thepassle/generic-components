a214644b66345572dfaa5c4649971422f72e3933
thepassle <pascalschilp@gmail.com>
2020-09-13 13:07:46 +0200

    feat: add global css for visually-hidden and skiplink

--- generic-skiplink/GenericSkiplink.js
+++ generic-skiplink/GenericSkiplink.js
@@ -1,3 +1,5 @@
+import { skiplink } from './skiplink.js';
+
 /**
  * @element generic-skiplink
  *
@@ -26,27 +28,11 @@ export class GenericSkiplink extends HTMLElement {
   render() {
     this.shadowRoot.innerHTML = `
       <style>
-        a {
-          background-color: white;
-          border-radius: 5px;
-          padding: 10px 20px 10px 20px;
-          position: absolute;
-          left: -999px;
-          width: 1px;
-          height: 1px;
-          top: auto;
-        }
-
-        a:focus {
-          top: 0px;
-          left: 0px;
-          height: auto;
-          width: auto;
-          margin: auto;
-        }
+        ${skiplink}
       </style>
 
       <a
+        skiplink
         part="anchor"
         class="skiplink"
         href="#${this.getAttribute('for')}">
--- index.js
+++ index.js
@@ -6,9 +6,11 @@ export { GenericDisclosure } from './generic-disclosure/GenericDisclosure.js';
 export { GenericListbox } from './generic-listbox/GenericListbox.js';
 export { GenericRadio } from './generic-radio/GenericRadio.js';
 export { GenericSkiplink } from './generic-skiplink/GenericSkiplink.js';
+export { skiplink } from './generic-skiplink/skiplink.js';
 export { GenericSwitch } from './generic-switch/GenericSwitch.js';
 export { GenericTabs } from './generic-tabs/GenericTabs.js';
 export { GenericVisuallyHidden } from './generic-visually-hidden/GenericVisuallyHidden.js';
+export { visuallyHidden } from './generic-visually-hidden/visually-hidden.js';
 // utils
 export { EventTargetShim } from './utils/EventTargetShim.js';
 export { SelectedMixin } from './utils/SelectedMixin.js';

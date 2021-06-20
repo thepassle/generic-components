fe12b260f6060a570bd7f86ed7a8aca5316f1dbc
thepassle <pascalschilp@gmail.com>
2021-06-20 20:43:55 +0200

    docs: add legacy docs

--- legacy/GenericListbox.jsx
+++ legacy/GenericListbox.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useRef } from "react";
 import "@generic-components/components/listbox.js";
 
-export function GenericListbox({ children, onSelectedChanged, selected }) {
+export function GenericListbox({
+  children,
+  onSelectedChanged,
+  selected,
+  label
+}) {
   const ref = useRef(null);
 
   /** Event listeners - run once */
@@ -12,6 +17,17 @@ export function GenericListbox({ children, onSelectedChanged, selected }) {
     }
   }, []);
 
+  /** Attributes - run whenever an attr has changed */
+
+  useEffect(() => {
+    if (
+      label !== undefined &&
+      ref.current.getAttribute("label") !== String(label)
+    ) {
+      ref.current.setAttribute("label", label);
+    }
+  }, [label]);
+
   /** Properties - run whenever a property has changed */
 
   useEffect(() => {
@@ -20,5 +36,9 @@ export function GenericListbox({ children, onSelectedChanged, selected }) {
     }
   }, [selected]);
 
-  return <generic-listbox ref={ref}>{children}</generic-listbox>;
+  return (
+    <generic-listbox ref={ref} label={label}>
+      {children}
+    </generic-listbox>
+  );
 }
